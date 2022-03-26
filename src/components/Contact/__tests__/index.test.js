cfd59338738178564b0e7462a24ef836fd9fb8e2
svnday <40030806+svnday@users.noreply.github.com>
2022-03-26 17:14:32 -0500

    tests and navbar config created

---
+++ src/components/Contact/__tests__/index.test.js
@@ -0,0 +1,31 @@
+/* eslint-disable jest/no-identical-title */
+/* eslint-disable testing-library/prefer-screen-queries */
+// __tests__/Contact.test.js
+import React from 'react'
+import { render, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Contact from './Contact'
+
+afterEach(cleanup)
+
+describe('Contact component renders', () => {
+  it('renders', () => {
+  render(<Contact />);
+});
+
+  it('renders', () => {
+    const { asFragment } = render(<Contact />)
+    expect(asFragment()).toMatchSnapshot()
+  })
+})
+
+
+it('renders', () => {
+  const { getByTestId } = render(<Contact />)
+  expect(getByTestId('h1tag')).toHaveTextContent('Contact me')
+})
+ 
+it('renders', () => {
+  const { getByTestId } = render(<Contact />)
+  expect(getByTestId('button')).toHaveTextContent('Submit')
+})
