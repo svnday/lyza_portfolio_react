858f115784692df29ba04355ab7284f75a28cff2
svnday <40030806+svnday@users.noreply.github.com>
2022-04-03 16:44:44 -0500

    tests and deployment to gh pages

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Modal({currentPhoto}) {
+const Modal = ({onClose, currentPhoto}) => {
     const {name, category, description, index} = currentPhoto;
 
 
@@ -8,10 +8,9 @@ function Modal({currentPhoto}) {
         <div className="modalBackgrop">
             <div className="modalContainer">
                 <h3 className="modalTitle">{name}</h3>
-                <img alt="current category" />
                 <img src={require(`../../assets/large/${category}/${index}.jpg`)} alt="current category" />
                 <p> {description} </p>
-                <button type="button">
+                <button onClick={onClose} type="button">
                     Close this modal
                 </button>
             </div>
